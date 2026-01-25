aa78eba92e97c86d15411706a86f4138098eb320
Andrei Regiani <andrei.cpp@gmail.com>
2026-01-25 18:24:04 -0300

    prettier lint

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const knownTextFiles = new Set(require('known-text-files'))
 function isTextFile(filename) {
   if (typeof filename !== 'string') return false
   filename = filename.replaceAll('\\', '/')
-  let entry = filename.slice(filename.lastIndexOf('/') + 1).trim().toLowerCase()
+  let entry = filename
+    .slice(filename.lastIndexOf('/') + 1)
+    .trim()
+    .toLowerCase()
   if (knownTextFiles.has(entry)) return true
   entry = entry.slice(entry.lastIndexOf('.'))
   return knownTextFiles.has(entry)
