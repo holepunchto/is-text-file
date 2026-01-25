017161b3a50e09adf4f271c4481f8816b8ed77f3
Andrei Regiani <andrei.cpp@gmail.com>
2026-01-25 16:02:17 -0300

    Undo isTextFile

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const knownTextFiles = new Set(require('known-text-files'))
 
 function isTextFile(filename) {
   if (typeof filename !== 'string') return false
-  const base = filename.trim().replaceAll('\\', '/').split('/').pop()
-  const lastDot = base.lastIndexOf('.')
-  const name = lastDot === -1 ? base : base.slice(lastDot)
-  return (
-    knownTextFiles.has(base.toLowerCase()) ||
-    knownTextFiles.has(name.toLowerCase())
-  )
+
+  const name = // TODO
+
+  return knownTextFiles.has(name.toLowerCase())
 }
 
 module.exports = isTextFile
