dd0b7377475684efce4fea9c8518663a918a0840
Andrei Regiani <andrei.cpp@gmail.com>
2026-01-25 18:02:31 -0300

    Refine isTextFile

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const knownTextFiles = new Set(require('known-text-files'))
 
 function isTextFile(filename) {
   if (typeof filename !== 'string') return false
-  const entry = filename.slice(filename.lastIndexOf('.'))
-  return knownTextFiles.has(entry.toLowerCase())
+  let entry = filename.toLowerCase().trim()
+  if (knownTextFiles.has(entry)) return true
+  entry = entry.slice(entry.lastIndexOf('.'))
+  return knownTextFiles.has(entry)
 }
 
 module.exports = isTextFile
