a30c2fdf3929af6b72371e5c8ce69180ba6dd7c6
Andrei Regiani <andrei.cpp@gmail.com>
2026-01-25 20:51:48 -0300

    Refactor

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const knownTextFiles = new Set(require('known-text-files'))
 
 function isTextFile(filename) {
   if (typeof filename !== 'string') return false
-  filename = filename.replaceAll('\\', '/')
-  let entry = filename
-    .slice(filename.lastIndexOf('/') + 1)
-    .trim()
-    .toLowerCase()
-  if (knownTextFiles.has(entry)) return true
-  entry = entry.slice(entry.lastIndexOf('.'))
+  let index = filename.lastIndexOf('.')
+  if (index === -1) index = filename.lastIndexOf('/')
+  if (index === -1) index = filename.lastIndexOf('\\')
+  if (index === -1) index = 0
+  if (filename[index] === '/' || filename[index] === '\\') index += 1
+  const entry = filename.slice(index).trim().toLowerCase()
   return knownTextFiles.has(entry)
 }
 
