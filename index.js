7605dba0ec17e7fd6e5f47ce59ce036cc5685651
Andrei Regiani <andrei.cpp@gmail.com>
2026-01-25 17:09:13 -0300

    Basic coverage

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const knownTextFiles = new Set(require('known-text-files'))
 
 function isTextFile(filename) {
   if (typeof filename !== 'string') return false
-
-  const name = // TODO
-
-  return knownTextFiles.has(name.toLowerCase())
+  const entry = filename.slice(filename.lastIndexOf('.'))
+  return knownTextFiles.has(entry.toLowerCase())
 }
 
 module.exports = isTextFile
