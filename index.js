b7c145034b2eb9e3d7af72dd8605e5213c562f11
Andrei Regiani <andrei.cpp@gmail.com>
2026-01-26 01:31:57 -0300

    || -> |

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function isTextFile(specifier) {
   if (typeof specifier !== 'string') return false
   let index = specifier.lastIndexOf('.')
   if (index === -1) {
-    index = ~(~specifier.lastIndexOf('/') || ~specifier.lastIndexOf('\\')) + 1
+    index = ~(~specifier.lastIndexOf('/') | ~specifier.lastIndexOf('\\')) + 1
   }
   return known.has(specifier.slice(index).trim().toLowerCase())
 }
